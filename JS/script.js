47fd034107f4770748d704bdcbc37e185b411105
gabrieleggiusti <giusti98@gmail.com>
2022-04-06 16:56:03 +0200

    Commit

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,12 @@
+
+/* Richiesta dati */
 const nkm = prompt ('Quanti chilometri devi percorrere?');
 const etaPasseggero = prompt ('Qual è la tua età?');
+
+/* Calcolo prezzo biglietto */
+let biglietto = nkm * 0.21;
+
+if (etaPasseggero < 18) {
+  biglietto = 
+}
 
